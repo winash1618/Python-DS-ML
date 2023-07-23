ef60708169a2468bd5639d9b4f77e7143444deaf
winash1618 <asharafbarcalona786@gmail.com>
2023-07-23 14:29:24 +0400

    [ds : module 03 ex00 and ex01 completed]

--- Piscine Python for datascience/Piscine Python for datascience - 3/ex00/tester.py
+++ Piscine Python for datascience/Piscine Python for datascience - 3/ex00/tester.py
@@ -1,13 +1,24 @@
-from S1E9 import Character, Stark
-Ned = Stark("Ned")
-print(Ned.__dict__)
-print(Ned.is_alive)
-Ned.die()
-print(Ned.is_alive)
-print(Ned.__doc__)
-print(Ned.__init__.__doc__)
-print(Ned.die.__doc__)
-print("---")
-Lyanna = Stark("Lyanna", False)
-print(Lyanna.__dict__)
-4
+# from S1E9 import Character, Stark
+# Ned = Stark("Ned")
+# print(Ned.__dict__)
+# print(Ned.is_alive)
+# Ned.die()
+# print(Ned.is_alive)
+# print(Ned.__doc__)
+# print(Ned.__init__.__doc__)
+# print(Ned.die.__doc__)
+# print("---")
+# Lyanna = Stark("Lyanna", False)
+# print(Lyanna.__dict__)
+
+
+from S1E9 import Character
+
+try:
+    hodor = Character("hodor")
+    
+except Exception as e:
+    print("Type Error:", e)
+
+
+
