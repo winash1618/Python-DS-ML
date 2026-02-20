d828b82b10ff8f5436ee594abb78fdf174b83d7e
Muhammed Asharaf K V <51042907+winash1618@users.noreply.github.com>
2026-02-20 15:29:19 +0400

    merge: PR #25
    Auto-merge for linked issue closure

--- ML Modules/ML Module 02/ex06/testUnivariate.py
+++ ML Modules/ML Module 02/ex06/testUnivariate.py
@@ -5,8 +5,9 @@ test
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from pathlib import Path
 from univariate_linear_model import MyLinearRegression as MyLR
-data = pd.read_csv("spacecraft_data.csv")
+data = pd.read_csv(Path(__file__).resolve().parent / "spacecraft_data.csv")
 X = np.array(data[['Age']])
 Y = np.array(data[['Sell_price']])
 myLR_age = MyLR(thetas = np.array([[1000.0], [-1.0]]), alpha = 2.5e-5, max_iter = 100000)
